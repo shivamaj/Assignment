a2285d69048e5a27b1d4d5f532eea9fd69afbf09
shivamaj <shivam.aj@gmail.com>
2020-07-12 17:03:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Chart from "./chart";
+
+const styles = theme => ({
+  "chart-container": {
+    height: 600
+  }
+});
+
+class App extends React.Component {
+  state = {
+    lineChartData: {
+      labels: [],
+      datasets: [
+        {
+          type: "line",
+          label: "BTC-USD",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: []
+        }
+      ]
+    },
+    lineChartOptions: {
+      responsive: true,
+      maintainAspectRatio: false,
+      tooltips: {
+        enabled: true
+      },
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              autoSkip: true,
+              maxTicksLimit: 10
+            }
+          }
+        ]
+      }
+    }
+  };
+
+  componentDidMount() {
+    const subscribe = {
+      type: "subscribe",
+      channels: [
+        {
+          name: "ticker",
+          product_ids: ["BTC-USD"]
+        }
+      ]
+    };
+
+    this.ws = new WebSocket("wss://ws-feed.pro.coinbase.com");
+
+    this.ws.onopen = () => {
+      this.ws.send(JSON.stringify(subscribe));
+    };
+
+    this.ws.onmessage = e => {
+      const value = JSON.parse(e.data);
+      if (value.type !== "ticker") {
+        return;
+      }
+
+      const oldBtcDataSet = this.state.lineChartData.datasets[0];
+      const newBtcDataSet = { ...oldBtcDataSet };
+      newBtcDataSet.data.push(value.price);
+
+      const newChartData = {
+        ...this.state.lineChartData,
+        datasets: [newBtcDataSet],
+        labels: this.state.lineChartData.labels.concat(
+          new Date().toLocaleTimeString()
+        )
+      };
+      this.setState({ lineChartData: newChartData });
+    };
+  }
+
+  componentWillUnmount() {
+    this.ws.close();
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes["chart-container"]}>
+        <Chart
+          data={this.state.lineChartData}
+          options={this.state.lineChartOptions}
+        />
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(App);
